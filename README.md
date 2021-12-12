a6d8ab9d8d929af5134b719239df4aee7a195d2e
inaciolimaf <92096585+inaciolimaf@users.noreply.github.com>
2021-12-12 17:36:41 -0300

    Organizando

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Tecnicas-prog-JAVA
- Repositório com os exercícios de JAVA
+ Repositório com os exercícios de JAVA da disciplina de Técnicas de Programação
--- lista4/Questao5/Principal.java
+++ lista4/Questao5/Principal.java
@@ -2,6 +2,21 @@ package Questao5;
 
 import java.util.Scanner;
 
+/*
+5) Escreva um programa completo para jogar o jogo da velha. Para tanto crie
+uma classe Jogo da Velha:
+a. A classe deve conter como dados privados um array bidimensional
+3x3 para representar a grade do jogo
+b. Utilize uma variável do tipo String para representar as possibilidades
+de ocupação de uma casa na grade (vazia, jogador 1 ou jogador 2)
+c. O construtor deve inicializar a grade como vazia
+d. Forneça um método para exibir a grade
+e. Permitir dois jogadores humanos
+f. Forneça um método para jogar o jogo; todo movimento deve ocorrer
+em uma casa vazia; depois de cada movimento, determine se houve
+uma derrota ou um empate.
+*/
+
 public class Principal {
     public static int lerInt() {
         Scanner leitor = new Scanner(System.in);
